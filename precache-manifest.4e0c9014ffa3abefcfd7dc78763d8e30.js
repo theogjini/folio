00d1e9f86821a6ac3182b69c833a014edbe47e01
Theo Gjini <theogjini@gmail.com>
2020-06-04 08:32:14 -0400

    Updates

---
+++ precache-manifest.4e0c9014ffa3abefcfd7dc78763d8e30.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716b9f015cc74b6490b14d57832d912d",
+    "url": "/folio/index.html"
+  },
+  {
+    "revision": "100e21f1dd28516dc901",
+    "url": "/folio/static/js/2.849a8809.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/folio/static/js/2.849a8809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bd2abf14a604c2506f",
+    "url": "/folio/static/js/main.60d10976.chunk.js"
+  },
+  {
+    "revision": "e32a669c9f241bf2599a",
+    "url": "/folio/static/js/runtime-main.51a8e260.js"
+  },
+  {
+    "revision": "90ec7b2ea64c725b32a32455bbe6e7a0",
+    "url": "/folio/static/media/Liane.90ec7b2e.png"
+  },
+  {
+    "revision": "ccb8a8bc12f3ff72a96b0f1544a1e867",
+    "url": "/folio/static/media/arrow.ccb8a8bc.svg"
+  },
+  {
+    "revision": "ef435be42036e3f1673ca940c4e2d89f",
+    "url": "/folio/static/media/dave.ef435be4.jpg"
+  },
+  {
+    "revision": "2646511bbf3ddf1824f6f0feceed489a",
+    "url": "/folio/static/media/guithub.2646511b.jpg"
+  },
+  {
+    "revision": "b2af66b2b824353c40e672c8447d9ce4",
+    "url": "/folio/static/media/lmdc.b2af66b2.png"
+  },
+  {
+    "revision": "569902427ca880e9fb141a9dd2d31c22",
+    "url": "/folio/static/media/lvl.56990242.png"
+  }
+]);
